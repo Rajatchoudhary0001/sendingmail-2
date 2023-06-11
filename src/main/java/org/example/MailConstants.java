6cc008ae37ecff837acf2d0028144ae08379488d
Rajat <rajatchoudhary9484@gmail.com>
2023-06-11 21:08:48 +0530

    sending mail

---
+++ src/main/java/org/example/MailConstants.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MailConstants {
+
+    public static final String SENDER = "rajatchaudhary7550@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "rakesh.kalita259@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
